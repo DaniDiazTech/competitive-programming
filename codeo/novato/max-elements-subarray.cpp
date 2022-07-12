bd3928808500a9eee6051c086939cfdc072cc6f7
Daniel Diaz <danieldiazgonza52@gmail.com>
2022-07-11 23:43:06 -0500

    Template update and exercise

--- codeo/novato/max-elements-subarray.cpp
+++ codeo/novato/max-elements-subarray.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main(){
-    // Only local. Comment out if the online judge doesn't use the flag
     #ifndef LOCAL
         // For getting input from input.txt file
         freopen("input.txt", "r", stdin);
@@ -11,11 +10,26 @@ int main(){
         // Printing the Output to output.txt file
         freopen("output.txt", "w", stdout);
     #endif
-    int n; 
+    int n, l, r, c; 
     cin >> n;
     
     int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cin >> c;
+
+    for (int i = 0; i < c; i++){
+        cin >> l >> r;
+        int building, maxh;
+        building = maxh = 0;
+        for (int j = l; j < r + 1; j++){
+            if (arr[j] > maxh){
+                maxh = arr[j];
+                building = j;
+            }
+        }
+        cout << building << endl;
+    }
 
 
     return 0;
