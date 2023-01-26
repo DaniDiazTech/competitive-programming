16c3cd0c5a28cda0b52cf76cd7d240914176bf3b
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-01-26 00:25:33 -0500

    Added Project euler folder

---
+++ project-euler/1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define ll long long
+
+int main(){
+  fastInp;
+  #if LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll sum = 0; 
+
+  auto start = chrono::system_clock::now();
+
+  // Brute force
+  for (int i = 1; i < 1000; i++)
+    sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+
+  auto end = chrono::system_clock::now();
+  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns" << std::endl;
+  cout << "Brute force: " << sum << endl;    
+
+
+  start = chrono::system_clock::now();
+
+  // Sieve-like approach  
+  sum = 0;
+  bool sieve[2001];
+  int j = 3, k = 5;
+  while (j < 1000){
+    sieve[j] = 1;
+    sum += j;
+    j +=3;
+  }
+  while (k < 1000){
+    if (!sieve[k])
+      sum += k;
+    sieve[k] = 1;
+    k +=5;
+  }
+
+  end = chrono::system_clock::now();
+
+  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns" << std::endl;
+  cout << "Sieve : " << sum << endl;    
+
+  return 0;
+}
