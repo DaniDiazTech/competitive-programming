9b6972c33eb630db587527ae6a1fc29271f376f9
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-04-03 11:20:48 -0500

    Modified template

--- template.cpp
+++ template.cpp
@@ -5,21 +5,23 @@ using namespace std;
 #define endl '\n'
 #define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define forn(i, n) for (int i = 0; i < n; i++) // for in range in python
-#define FOR(i, a, b) for (int i = a; i < b; i++) // for in range in python
+#define fore(i, a, b) for (int i = a; i < b; i++) // for in range in python
 #define int long long int
 #define double long double
 #define pb push_back
 #define ff first
 #define ss second
 #define mk make_pair
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size() 
 
 typedef pair<int, int> pii;
 
 const int MAX = 1e6;
 const int MIN = -MAX;
-const int INF = LLONG_MAX;
-const int MINF = LLONG_MIN;
-const int MOD = 1e9 + 7;
+const int oo = LLONG_MAX;
+const int ooo = LLONG_MIN;
+const int mod = 1e9 + 7;
 
 // int arr[MAX];
 void solve(){
