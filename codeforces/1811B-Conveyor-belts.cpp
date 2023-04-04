0d2f9cbbf0e0839bb70e2a5ed03164fce0d5cd77
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-04-04 13:56:06 -0500

    CF 863 Div 3

---
+++ codeforces/1811B-Conveyor-belts.cpp
@@ -0,0 +1,48 @@
+// Made by Daniel Diaz (@Danidiaztech)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define forn(i, n) for (int i = 0; i < n; i++) // for in range in python
+#define fore(i, a, b) for (int i = a; i < b; i++) // for in range in python
+#define int long long int
+#define double long double
+#define pb push_back
+#define ff first
+#define ss second
+#define mk make_pair
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size() 
+
+typedef pair<int, int> pii;
+
+const int MAX = 1e6;
+const int MIN = -MAX;
+const int oo = LLONG_MAX;
+const int ooo = LLONG_MIN;
+const int mod = 1e9 + 7;
+
+// int arr[MAX];
+void solve(){
+  int n, x1, y1, x2, y2;
+  cin >> n >> x1 >> y1 >> x2 >> y2;
+  cout<< abs(min(min(x1, n + 1 - x1), min(y1, n + 1 - y1)) - min(min(x2, n + 1 - x2), min(y2, n + 1 - y2))) << endl;
+}
+
+int32_t main() {
+  fastInp;
+  #if LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int tc = 1;
+  cin >> tc;
+
+  for (int t = 1; t <= tc; t++){
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+  return 0;
+}
