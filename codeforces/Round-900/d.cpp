64787c986274a8af998dce2a486b0822b0f98702
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-09-26 11:52:46 -0500

    Added latest round

---
+++ codeforces/Round-900/d.cpp
@@ -0,0 +1,89 @@
+// Made by Daniel Diaz (@Danidiaztech)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define forn(i, n) for (int i = 0; i < n; i++) // for in range in python
+#define fore(i, a, b) for (int i = a; i < b; i++) // for in range in python
+#define int long long int
+#define double long double
+#define pb push_back
+#define ff first
+#define ss second
+#define mk make_pair
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size() 
+
+typedef pair<int, int> pii;
+typedef vector<int> vii;
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2> &p){
+    os << p.first <<  " " << p.second;
+    return os;
+}
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &v){
+    for(auto const &i: v){
+        os<<i<<" ";
+    }
+    os<<'\n';
+    return os;
+}
+
+const int MAX = 1e6;
+const int MIN = -MAX;
+const int oo = LLONG_MAX / 2;
+const int ooo = LLONG_MIN / 2;
+const int mod = 1e9 + 7;
+const string yes = "YES", no = "NO";
+
+
+int n, t[4*MAX];
+
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+
+int andd(int v, int tl, int tr, int l, int r) {
+    if (l > r) 
+        return 0;
+    if (l == tl && r == tr) {
+        return t[v];
+    }
+    int tm = (tl + tr) / 2;
+    return andd(v*2, tl, tm, l, min(r, tm))
+           & andd(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
+
+
+// int dp[MAX];
+void solve(){
+  cout << (1ll << 34) - 1 << endl;
+ 
+}
+
+int32_t main() {
+  fastInp;
+  #if LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int tc = 1;
+  // cin >> tc;
+// 
+  for (int t = 1; t <= tc; t++){
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+  return 0;
+}
