d30a8d9c67feb925ef8fda9f583fc275931fe92b
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-10-27 23:54:10 -0500

    Finally solved high score

--- cses/Graphs/Flight-routes.cpp
+++ cses/Graphs/Flight-routes.cpp
@@ -2,79 +2,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl '\n'
-#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define forn(i, n) for (int i = 0; i < n; i++) // for in range in python
-#define fore(i, a, b) for (int i = a; i < b; i++) // for in range in python
-#define int long long int
-#define double long double
-#define pb push_back
-#define ff first
-#define ss second
-#define mk make_pair
-#define all(x) x.begin(),x.end()
-#define sz(x) (int)x.size() 
+#define ll long long
 
-typedef pair<int, int> pii;
-typedef vector<int> vii;
-
-const int MAX = 1e6;
-const int MIN = -MAX;
-const int oo = LLONG_MAX / 2;
-const int ooo = LLONG_MIN / 2;
 const int mod = 1e9 + 7;
+const string yes = "YES", no = "NO";
 
-vector<pii> g[MAX];
-int dis[MAX];
-void solve(){
-  int n, m, k; cin >> n >> m >> k;
-  vii ans;
-  forn(i,m){
-    int a, b, c; cin >>a >> b >> c; 
-    g[a].pb({c, b});
+const int N = 1e6;
+vector< pair <int, int> > g[N];
+int vis[N];
+multiset<int> ans[N];
+
+int n, m, k;
+
+multiset<int> go( int u ){
+  if (u == 1){
+    multiset<int> s;
+    s.insert(0);
+    return ans[u] = s;
   }
 
-  for (int i =0 ; i<=n; i++) dis[i] = oo;
+  vis[u] = 1;
+  multiset<int> pq = ans[u];
 
-  // Dijkstra
-  priority_queue<pii> pq;
-  pq.push({0, 1});
-  dis[1] = 0;
-
-  while (!pq.empty()){
-    pii p = pq.top(); pq.pop();
-    int w = p.ff, u = p.ss;
-    for (auto x: g[u]){
-      // cout << x.ss << " " << x.ff << endl;
-      if (dis[x.ss] > dis[u] + x.ff){
-        // Better answer found, relax
-        pq.push({-x.ff, x.ss});
-        dis[x.ss] = dis[u] + x.ff;
+  for ( auto x: g[u] ){
+    if ( vis[x.first] == 1) continue;
+    auto p = go(x.first);
+  
+    for ( auto l: p ){
+      int w = l + x.second;
+      cout << "U: " << u << " V: " << x.first << " W: " << w << endl;
+      if (pq.size() < k){
+        pq.insert(w);
       }
-      if (x.ss == n){
-        // cout << "HERE" << endl;
-        ans.pb(dis[x.ss]);
+      else{
+        cout << "W: " << w << " rbegin() : " << *(pq.rbegin()) << endl;
+        if (*(pq.rbegin()) > w){
+          auto it = pq.end();
+          it--;
+          pq.erase(it);
+          pq.insert(w);
+        }
+        else{
+          break;
+        }
       }
     }
   }
-  sort(ans.begin(), ans.end());
-  for (int i = 0; i < k; i++){
-    cout << ans[i] << " ";
+  vis[u] = 2;
+
+  return ans[u] = pq;
+}
+
+void solve(){
+  cin >> n >> m >> k;
+
+  for (int i = 0 ;i < m; i++){
+    // invert edges
+    int a, b, c; cin >> a >> b >> c;
+    g[b].push_back({a, c});
+  }
+  auto q =   go(n) ;
+  for (auto x : q) cout << x  << " ";
+  cout << '\n';
+  for (int i = 1; i <= n; i++){
+    for (auto x: ans[i]) cout << x << " ";
+    cout << endl;
   }
 }
 
-int32_t main() {
-  fastInp;
+int main() {
+  cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+
   #if LOCAL
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
   #endif
 
   int tc = 1;
+  // cin >> tc;
 
   for (int t = 1; t <= tc; t++){
-    // cout << "Case #" << t << ": ";
     solve();
   }
-  return 0;
 }
--- cses/Graphs/High-score.cpp
+++ cses/Graphs/High-score.cpp
@@ -2,87 +2,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl '\n'
-#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define forn(i, n) for (int i = 0; i < n; i++) // for in range in python
-#define fore(i, a, b) for (int i = a; i < b; i++) // for in range in python
-#define int long long int
-#define double long double
-#define pb push_back
-#define ff first
-#define ss second
-#define mk make_pair
-#define all(x) x.begin(),x.end()
-#define sz(x) (int)x.size() 
+#define ll long long
 
-typedef pair<int, int> pii;
-
-const int MAX = 1e6;
-const int MIN = -MAX;
-const int oo = LLONG_MAX;
-const int ooo = LLONG_MIN;
 const int mod = 1e9 + 7;
+const string yes = "YES", no = "NO";
 
-int vis[4000], vis2[4000];
-vector<int> g[4000], g2[4000];
-int dis[4000 + 10];
+const int N = 3000;
+const ll inf = 1e15;
+
+vector<pair<ll,int>> g[N];
+vector<int> g2[N];
+bool vis[N];
+
+set<int> possible;
+
 void dfs(int u){
-  vis[u] =1;
-  for (auto v: g[u]){
-    if (!vis[v])
-      dfs(v);
-  }
-}
-void dfs2(int u){
-  vis2[u] =1;
+  vis[u] = 1;
+  possible.insert(u);
   for (auto v: g2[u]){
-    if (dis[v] == oo){
-      cout << - 1<< endl;
-      exit(0);
-    }
-    if (!vis2[v])
-      dfs2(v);
+    if (vis[v]) continue;
+    dfs(v);
   }
 }
+
+
+
+
 
 void solve(){
-  int n,m;
+  int n, m;
+
   cin >> n >> m;
-  fill(dis, dis + n + 5, ooo / 2);
-  vector<vector<int>> edges;
 
-  fore(i,0,m){
-    vector<int> v(3); cin >> v[0] >> v[1] >> v[2];
-    g[v[0]].pb(v[1]);
-    g2[v[1]].pb(v[0]);
-    edges.pb(v);
+  for (int i = 0;i < m; i++){
+    int a,b;
+    ll w;
+    cin >> a >> b >> w;
+    g[a].push_back({w, b});
+    g2[b].push_back(a);
   }
-  dfs(1);
 
-  dis[1] = 0;
-  // Bellman ford algorithm
-  for (int i = 0; i < n - 1; i++){
-    for (auto e: edges){
-      if (vis[e[0]])
-        dis[e[1]] = max(dis[e[1]], dis[e[0]] + e[2]); 
-    }
-  }
-  // for (int i =0 ;i < n; i++) cout << dis[i + 1] << endl;;
-  // Perform bellman ford twice
-  for (int i = 0; i < n - 1; i++){
-    for (auto e: edges){
-      if (dis[e[1]] < dis[e[0]] + e[2] && vis[e[0]] && dis[e[0]] != oo){
-        // Found cycle
-        dis[e[1]] = oo;
+  // check elements that reach n
+  dfs(n);
+
+  // spfa
+  queue<int> q;
+  vector<ll> d(n + 1, -inf), cnt(n + 1, 0);
+  vector<bool> inqueue(n + 1);
+
+  d[1] = 0;
+  inqueue[1] = 1;
+  q.push(1);
+
+  bool ok = 1;
+  while (!q.empty()){
+    int u = q.front();
+    q.pop();
+    inqueue[u] = false;
+    for (auto v: g[u]){
+      if (!possible.count(v.second)) continue;
+      ll x = d[u] + v.first;
+      // maximize distance
+      if (x > d[v.second]){
+        d[v.second] = x;
+
+        if (!inqueue[v.second]){
+          q.push(v.second);
+          cnt[v.second]++;
+          inqueue[v.second] = true;
+          if (cnt[v.second] > n + 1){
+            // found positive cycle
+            ok = 0;
+            while (!q.empty()) q.pop();
+            break;
+          }
+        }
       }
     }
   }
-  dfs2(n);
-  cout << dis[n] << endl;
+
+  if (ok){
+    cout << d[n] << '\n';
+  }
+  else{
+    cout << -1 << '\n';
+  }
+
 }
 
-int32_t main() {
-  fastInp;
+int main() {
+  cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+
   #if LOCAL
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -92,8 +102,6 @@ int32_t main() {
   // cin >> tc;
 
   for (int t = 1; t <= tc; t++){
-    // cout << "Case #" << t << ": ";
     solve();
   }
-  return 0;
 }
